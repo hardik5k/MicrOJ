8247e0a5b16c757494ac8e56d3be45527f5408d8
Hardik Khandelwal <khandelwalhk02@gmail.com>
2023-11-17 18:52:42 +0530

    added message queue

---
+++ Server/rabbitMQ.js
@@ -0,0 +1,33 @@
+const amqp = require('amqp-connection-manager');
+const queueName = "OJq"
+
+const connection = amqp.connect(['amqp://localhost']);
+connection.on('connect', () => console.log('Connected to RabbitMQ server'));
+connection.on('disconnect', err => console.log('Disconnected from RabbitMQ server', err));
+
+
+const channel = connection.createChannel({
+    json: true,
+    setup: channel => {
+                return channel.assertQueue(queueName, {
+                    durable: true
+                })
+            }
+});
+
+async function sendMessage(data){
+    let res = -1;
+    await channel.sendToQueue(queueName, JSON.stringify(data))
+    .then(()=>{ 
+        console.log("Message Queued Succesfully");
+        res = 200;
+    })
+    .catch((err) => {
+        console.log(`System Error: ${err.stack.toString()}`);
+        res = 500;
+    });
+
+    return res;
+};
+
+module.exports = {sendMessage};
