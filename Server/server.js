b8494bcd76052f136b1498a9409de10fa8afbe46
Hardik Khandelwal <khandelwalhk02@gmail.com>
2023-11-18 19:17:31 +0530

    added check submission route

--- Server/server.js
+++ Server/server.js
@@ -6,11 +6,13 @@ const port = 3000;
 const mongoose = require('mongoose');
 const testCases = require('./routes/testCase.js');
 const submit = require('./routes/submit.js');
+const result = require('./routes/result.js');
 
 
 
 app.use('/testcase', testCases);
 app.use('/submit', submit);
+app.use('/result', result);
 
 
 app.get('/', (req, res) => {
